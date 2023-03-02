6540837170581520618e44e7356c39d9eb3c7834
cengiztuglu <cngztuglu@gmail.com>
2023-03-03 02:06:30 +0300

    Add project files.

---
+++ EntityLayer/Concrete/About.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class About
+    {
+        public int MyProperty { get; set; }
+        
+    }
+}
