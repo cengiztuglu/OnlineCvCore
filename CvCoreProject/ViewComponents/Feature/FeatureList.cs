83a0f5b1a78264dae967c9fa1a346a5c3ec43705
cengiztuglu <cngztuglu@gmail.com>
2023-03-16 04:07:16 +0300

    FeatureList

--- CvCoreProject/ViewComponents/Feature/FeatureList.cs
+++ CvCoreProject/ViewComponents/Feature/FeatureList.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CvCoreProject.ViewComponents.Feature
 {
     public class FeatureList : ViewComponent
+
     {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = featureManager.TGetList();
+            return View(values);
         }
 
     }
